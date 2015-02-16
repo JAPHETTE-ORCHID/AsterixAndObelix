47c99619a7ed0306a21436c20ff4d95cea49618b
Ilian Kostov <ilian.kostov@gmail.com>
2015-02-16 20:22:29 +0200

    Create class Enemy

--- AsterixAndObelixRPG/AsterixAndObelixConsoleRPG/Models/Players/Enemy.cs
+++ AsterixAndObelixRPG/AsterixAndObelixConsoleRPG/Models/Players/Enemy.cs
@@ -1,20 +1,56 @@
-﻿namespace AsterixAndObelixConsoleRPG.Models.Players
+﻿using AsterixAndObelixConsoleRPG.Models.Items;
+
+namespace AsterixAndObelixConsoleRPG.Models.Players
 {
+    using System;
+    using System.Collections.Generic;
+
     using AsterixAndObelixConsoleRPG.Contracts;
     using AsterixAndObelixConsoleRPG.Enumerations;
 
     internal class Enemy : PlayerObject, IDrop
     {
-        private EnemyType enemyType;
-
-        public void DropRandomItem()
+        public Enemy(int attack, int defence, int health, EnemyType enemyType, int gold, List<IItem> itemsForDrop )
+            : base(attack, defence, health)
         {
-            throw new System.NotImplementedException();
+            this.EnemyType = enemyType;
+            this.Gold = gold;
+            this.ItemsForDrop = itemsForDrop;
         }
 
-        public void DropGold()
+        public EnemyType EnemyType { get; set; }
+
+        public int Gold { get; set; }
+
+        public List<IItem> ItemsForDrop { get; set; }
+
+        public IItem DropRandomItem()
         {
-            throw new System.NotImplementedException();
+            Random rnd = new Random();
+            int itemNumber = rnd.Next(1, 5);
+            IItem itemForDrop;
+            switch (this.EnemyType)
+            {
+                case EnemyType.Cadet:
+                    switch (itemNumber)
+                    {
+                        
+                        default:
+                            itemForDrop = new Sword(100, ItemType.Common, 100);
+                            break;
+                            
+                    }
+                    break;
+                default:
+                    itemForDrop = new Sword(100, ItemType.Common, 100);
+                    break;
+            }
+            return itemForDrop;
+        }
+
+        public int DropGold()
+        {
+            return this.Gold;
         }
     }
 }
