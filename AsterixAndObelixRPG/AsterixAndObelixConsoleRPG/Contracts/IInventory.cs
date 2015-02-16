f2b365288849fe08e0b723f70d5b194b48a5449b
dhadzhiew <dhadzhiew@gmail.com>
2015-02-16 21:32:37 +0200

    a

---
+++ AsterixAndObelixRPG/AsterixAndObelixConsoleRPG/Contracts/IInventory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsterixAndObelixConsoleRPG.Contracts
+{
+    public interface IInventory
+    {
+        void AddItem(IItem item);
+
+        void RemoveItem(IItem item);
+
+        string ShowItems();
+    }
+}
