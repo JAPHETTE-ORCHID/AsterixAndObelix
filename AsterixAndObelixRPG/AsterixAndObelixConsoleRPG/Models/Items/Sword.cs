0a8b0f64327b2beb7fd18e1cd100c53e5f531a18
valkone <valkb7b@gmail.com>
2015-02-16 19:34:33 +0200

    Sword added

---
+++ AsterixAndObelixRPG/AsterixAndObelixConsoleRPG/Models/Items/Sword.cs
@@ -0,0 +1,15 @@
+﻿using AsterixAndObelixConsoleRPG.Enumerations;
+
+namespace AsterixAndObelixConsoleRPG.Models.Items
+{
+    public class Sword : AttackItems
+    {
+        public Sword(int price, ItemType itemType, int attack)
+            : base(attack)
+        {
+            this.ItemType = itemType;
+            this.Price = price;
+
+        }
+    }
+}
