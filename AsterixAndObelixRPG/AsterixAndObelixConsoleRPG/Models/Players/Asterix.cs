dfd676c264c4b8344947f33ab2bd7979b6fe3aa1
IvanHristov <i.hristov95@gmail.com>
2015-02-18 17:41:56 +0200

    add Asterix.cs

---
+++ AsterixAndObelixRPG/AsterixAndObelixConsoleRPG/Models/Players/Asterix.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsterixAndObelixConsoleRPG.Models.Players
+{
+    class Asterix : Hero
+    {
+        public Asterix(string name, int attack, int defence, int health)
+            : base (name, attack, defence, health)
+        {   
+        }
+    }
+}
