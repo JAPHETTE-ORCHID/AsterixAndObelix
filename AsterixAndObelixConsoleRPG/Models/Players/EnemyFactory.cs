e414095ca1527935da3de57d4414421aad2fde0f
IvanHristov <i.hristov95@gmail.com>
2015-02-26 23:14:38 +0200

    new enemy stats

--- AsterixAndObelixConsoleRPG/Models/Players/EnemyFactory.cs
+++ AsterixAndObelixConsoleRPG/Models/Players/EnemyFactory.cs
@@ -16,13 +16,13 @@ namespace AsterixAndObelixConsoleRPG.Models.Players
                 case EnemyType.Manipularius:
                     return new Enemy(430, 400, 60, EnemyType.Manipularius, 1000);
                 case EnemyType.Tribune:
-                    return new Enemy(800, 750, 75, EnemyType.Tribune, 2000);
+                    return new Enemy(600, 500, 75, EnemyType.Tribune, 2000);
                 case EnemyType.Centurion:
-                    return new Enemy(1400, 1300, 85, EnemyType.Centurion, 3000);
+                    return new Enemy(750, 680, 85, EnemyType.Centurion, 3000);
                 case EnemyType.Ordinatus:
-                    return new Enemy(2050, 2050, 100, EnemyType.Ordinatus, 4000);
+                    return new Enemy(870, 830, 100, EnemyType.Ordinatus, 4000);
                 case EnemyType.Caesar:
-                    return new Enemy(3500, 3500, 1000, EnemyType.Caesar, 10000);
+                    return new Enemy(100000, 100000, 200, EnemyType.Caesar, 10000);
                 default:
                     throw new InputException("Invalid enemy type");
             }
